3776d4c83baae96101ca752f5717b3dbbaa03c15
Gavin Waako <gavin.sonny27@gmail.com>
2026-02-07 21:12:15 -0500

    Fix error not returning JSON objects

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-import fs from 'fs'
+import fs from "fs";
 
 function read_groceries(filepath) {
-    fs.readFile(filepath, "utf-8", (errors, contents) => {
-        let price_database = [];
-        for (let line of contents.split("\n")) {
-            let [name, price] = line.split(",");
-            price_database.push(name, price);
-        }
-        console.log(price_database);
-    })
+  fs.readFile(filepath, "utf-8", (errors, contents) => {
+    let price_database = [];
+    for (let line of contents.trim().split("\n")) {
+      let [name, price] = line.trim().split(",");
+      price_database.push({ name, price });
+    }
+    console.log(price_database);
+  });
 }
 
 read_groceries("groceries.txt");
-
